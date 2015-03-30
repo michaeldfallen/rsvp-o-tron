4ffae08ce61c25621255f5f236e0da8051eb5789
Michael Allen <michael@michaelallen.io>
2015-03-30 10:49:23 +0100

    hooked in wtforms for form validation

--- add_guest/add_guest.py
+++ add_guest/add_guest.py
@@ -1,17 +1,19 @@
 from views.template import Template
-from wtforms import Form, TextField, validators
+from flask_wtf import Form
+from wtforms import TextField, validators
 from flask import redirect, url_for
 
 
 class AddGuestForm(Form):
-    first_name = TextField('first_name', [validators.Length(min=3)])
-    last_name = TextField('last_name', [validators.Length(min=3)])
+    first_name = TextField('first_name', [validators.DataRequired()])
+    last_name = TextField('last_name', [validators.DataRequired()])
 
 
 class AddGuest(Template):
 
-    def __init__(self, invite_token):
+    def __init__(self, invite_token, form):
         self.token = invite_token
+        self.form = form
 
     def title(self):
         return "Add a guest"
@@ -20,8 +22,13 @@ class AddGuest(Template):
 def routes(app):
     @app.route('/invite/<token>/guest', methods=['POST'])
     def add_guest_to_invite(token):
-        return redirect(url_for('list_invites'))
+        form = AddGuestForm()
+        if form.validate_on_submit():
+            return redirect(url_for('list_invites'))
+        else:
+            return AddGuest(token, form).render()
 
     @app.route('/invite/<token>/guest/new', methods=['GET'])
     def add_guest_form(token):
-        return AddGuest(token).render()
+        form = AddGuestForm()
+        return AddGuest(token, form).render()
