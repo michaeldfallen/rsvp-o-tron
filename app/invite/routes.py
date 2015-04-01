3a0c3ca2de6984f04ae8869a4fde88fae7232988
Michael Allen <michael@michaelallen.io>
2015-04-01 14:51:17 +0100

    create invite module and move invite routes in to it

--- app/invite/routes.py
+++ app/invite/routes.py
@@ -3,28 +3,28 @@ from app.model import Invite
 from app import views
 
 
-def routes(app):
-    @app.route('/')
+def register_routes(blueprint):
+    @blueprint.route('/')
     def hello():
         return views.Home().render()
 
-    @app.route('/invite')
+    @blueprint.route('/invite')
     def list_invites():
         all_invites = Invite.all()
         return views.ListInvites(all_invites).render()
 
-    @app.route('/invite/new')
+    @blueprint.route('/invite/new')
     def create_invite_form():
         return views.CreateInvite().render()
 
-    @app.route('/invite/new', methods=['POST'])
+    @blueprint.route('/invite/new', methods=['POST'])
     def create_invite():
         invite = Invite()
         Invite.save(invite)
         return redirect(url_for('list_invites'))
 
-    @app.route('/invite/<token>/delete', methods=['POST'])
-    @app.route('/invite/<token>', methods=['DELETE'])
+    @blueprint.route('/invite/<token>/delete', methods=['POST'])
+    @blueprint.route('/invite/<token>', methods=['DELETE'])
     def delete_invite(token):
         Invite.delete(token)
         return redirect(url_for('list_invites'))
