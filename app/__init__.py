9b4edde25740cc504b5c0ad1e20a88d5b381536f
Michael Allen <michael@michaelallen.io>
2015-04-01 22:16:08 +0100

    create rsvp blueprint and module

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 # flake8: noqa
 from flask import Flask
 from flask.ext.script import Manager
-from app import invite, guest
+from app import invite, guest, rsvp
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.register_blueprint(guest.blueprint)
 app.register_blueprint(invite.blueprint)
+app.register_blueprint(rsvp.blueprint)
 
 manager = Manager(app)
 db.init(app, manager)
